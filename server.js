9af745f4a61e05416f571103e0bf27f0962d8d56
Bipin Swarnkar <vipinswarnkar1989@gmail.com>
2017-01-24 15:21:05 +0530

    make it better

--- server.js
+++ server.js
@@ -55,15 +55,4 @@ app.get('/*', function(req, res) {
 /**
  * Get port from environment and store in Express.
  */
-const port = process.env.PORT || '8080';
-app.set('port', port);
-
-/**
- * Create HTTP server.
- */
-const server = http.createServer(app);
-
-/**
- * Listen on provided port, on all network interfaces.
- */
-server.listen(port, () => console.log(`API running on localhost:${port}`));
+app.listen(process.env.PORT || 8080);

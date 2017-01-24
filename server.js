6b2877c7fba02d351530579d19b17fdf3f218a64
Bipin Swarnkar <vipinswarnkar1989@gmail.com>
2017-01-25 00:21:16 +0530

    make it better

--- server.js
+++ server.js
@@ -10,23 +10,6 @@ const api = require('./server/routes/api');
 
 const app = express();
 
-// If an incoming request uses
-// a protocol other than HTTPS,
-// redirect that request to the
-// same url but with HTTPS
-const forceSSL = function() {
-  return function (req, res, next) {
-    if (req.headers['x-forwarded-proto'] !== 'https') {
-      return res.redirect(['https://', req.get('Host'), req.url].join(''));
-    }
-    next();
-  }
-}
-
-// Instruct the app
-// to use the forceSSL
-// middleware
-app.use(forceSSL());
 
 
 

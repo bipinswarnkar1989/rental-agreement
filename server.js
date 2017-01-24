2375856f2a440af5920b085c1552c391e0354a5c
Bipin Swarnkar <vipinswarnkar1989@gmail.com>
2017-01-24 22:56:03 +0530

    make it better

--- server.js
+++ server.js
@@ -28,12 +28,12 @@ app.use(function(req, res, next) {
 app.use('/', api);
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + '/src'));
+app.use(express.static(__dirname + '/dist'));
 
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/src/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
 /**
